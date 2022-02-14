5d398aa9d218ddc1ed813028e84bf46b0e9e1480
kollo.magor.ors <kollo.magor.ors@accenture.com>
2022-02-14 18:44:47 +0200

    inception not working

--- depth-camera/object_coordinates.py
+++ depth-camera/object_coordinates.py
@@ -8,7 +8,7 @@ import pickle
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.detectNet("ssd-inception-v2", threshold=0.35)
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.3)
 #camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
