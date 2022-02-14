53431be71bf66185b47278dc6c95e0bfb863ee64
kollo.magor.ors <kollo.magor.ors@accenture.com>
2022-02-14 18:26:15 +0200

    new test

--- depth-camera/object_coordinates.py
+++ depth-camera/object_coordinates.py
@@ -66,23 +66,22 @@ while True:
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
     for object_detected in detections:
-        # if object_detected.ClassID in IDs:
-        #     continue
-        point = (int(object_detected.Center[0]), int(object_detected.Center[1]))
-        print(point)
-        cv2.circle(color_frame, point, 4, (255, 255, 0))
-        distance = depth_frame_distance.get_distance(point[0], point[1])
-        result = rs.rs2_deproject_pixel_to_point(intrinsics, [point[0], point[1]], distance)
-        x = result[2]
-        y = result[0]
-        z = result[1]
-        print("x = ", x)
-        print("y = ", y)
-        print("z = ", z)
-        cv2.putText(color_frame, "distance mm: {0:.3f}".format(x), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(color_frame, "y: {0:.3f}".format(y), (point[0], point[1] - 45), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(color_frame, "z: {0:.3f}".format(z), (point[0], point[1] - 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        
+        if object_detected.ClassID in IDs:
+            point = (int(object_detected.Center[0]), int(object_detected.Center[1]))
+            print(point)
+            cv2.circle(color_frame, point, 4, (255, 255, 0))
+            distance = depth_frame_distance.get_distance(point[0], point[1])
+            result = rs.rs2_deproject_pixel_to_point(intrinsics, [point[0], point[1]], distance)
+            x = result[2]
+            y = result[0]
+            z = result[1]
+            print("x = ", x)
+            print("y = ", y)
+            print("z = ", z)
+            cv2.putText(color_frame, "distance mm: {0:.3f}".format(x), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+            cv2.putText(color_frame, "y: {0:.3f}".format(y), (point[0], point[1] - 45), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+            cv2.putText(color_frame, "z: {0:.3f}".format(z), (point[0], point[1] - 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+            
 
     cv2.imshow("Color frame", color_frame)
     key = cv2.waitKey(1)
