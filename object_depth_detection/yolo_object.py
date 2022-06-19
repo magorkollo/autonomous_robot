3698f94e0ef0d007cdd25dc315a33be8e148fda2
magorors <magorors_kollo@yahoo.com>
2022-06-19 23:57:31 +0300

    headless + object_list under dev

--- object_depth_detection/yolo_object.py
+++ object_depth_detection/yolo_object.py
@@ -9,7 +9,7 @@ import math
 
 class YoloNet:
     def __init__(self):
-        self.net = cv2.dnn.readNet("/home/jnano/autonomous_robot/object_depth_detection/yolo_dependencies/yolov3.weights", "yolo_dependencies/yolov3.cfg")
+        self.net = cv2.dnn.readNet("/home/jnano/autonomous_robot/object_depth_detection/yolo_dependencies/yolov3-tiny.weights", "yolo_dependencies/yolov3-tiny.cfg")
         self.classes = []
         self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
         self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
@@ -17,15 +17,13 @@ class YoloNet:
             self.classes = [line.strip() for line in names.readlines()]
         self.output_layers = [layer_name for layer_name in self.net.getUnconnectedOutLayersNames()]
         self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+        self.start_time = time.time() 
     
-    def load_image(img_path):
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, None, fx=0.4, fy=0.4)
-        height, width, channels = img.shape
-        return img, height, width, channels
+    def init_depth_camera(self):
+        self.dc = DepthCamera()
     
     def start_webcam(self):
-        video = cv2.VideoCapture(0)
+        video = cv2.VideoCapture(2)
         return video
 
     def display_blob(blob):
@@ -61,6 +59,33 @@ class YoloNet:
                         y = int(center_y - h / 2)
                         boxes.append([x, y, w, h])
                         centers.append([center_x, center_y])
+                        confs.append(float(conf))
+                        class_ids.append(class_id)
+        return boxes, confs, class_ids, centers
+
+
+    def get_box_dimensions_depth(self, results, height, width):
+        boxes = []
+        confs = []
+        class_ids = []
+        centers = []
+        lefts = []
+        rights = []
+        for result in results:
+            for detect in result:
+                scores = detect[5:]
+                class_id = np.argmax(scores)
+                if class_id == 47:
+                    conf = scores[class_id]
+                    if conf > 0.25:
+                        center_x = int(detect[0] * width)
+                        center_y = int(detect[1] * height)
+                        w = int(detect[2] * width)
+                        h = int(detect[3] * height)
+                        x = int(center_x - w/2)
+                        y = int(center_y - h / 2)
+                        boxes.append([x, y, w, h])
+                        centers.append([center_x, center_y])
                         lefts.append([int(x+10), int(y+h/2)])
                         rights.append([int(x+w-10), int(y+h/2)])
                         confs.append(float(conf))
@@ -70,31 +95,17 @@ class YoloNet:
     def calculate_2d_distance(self, point0, point1):
         sum_y = point0[0] - point1[0]
         sum_z = point0[1] - point1[1]
-        # distance_2d = math.sqrt(pow(sum_y, 2) + pow(sum_z,2))
         distance_2d = math.sqrt(abs(pow(sum_y,2) - pow(sum_z,2)))
-        # print("value 0 ---y: ", abs(sum_y))
-        # print("value 0 ---z: ", abs(sum_z))
         return distance_2d
 
-    def calculate_depth(self, centers, lefts, rights):
+    def calculate_depth(self, left, right):
         distance_2d = 0
-        for i in range(len(centers)):
-            center = centers[i]
-            left = lefts[i]
-            right = rights[i]
-            depth_coord_center = dc.get_depth_coordinates(center[0], center[1])
-            depth_coord_left = dc.get_depth_coordinates(left[0], left[1])
-            depth_coord_right = dc.get_depth_coordinates(right[0], right[1])
-            left_deep = [depth_coord_left[0], depth_coord_left[1]]
-            right_deep = [depth_coord_right[0], depth_coord_right[1]]
-            distance_2d = calculate_2d_distance(left_deep, right_deep)
-            # print("depth distance: ", depth_coord_left[2] - depth_coord_right[2])
-            depth_center.append(depth_coord_center)
-            depth_right.append(depth_coord_right)
-            depth_left.append(depth_coord_left)
+        depth_coord_left = self.dc.get_depth_coordinates(left[0], left[1])
+        depth_coord_right = self.dc.get_depth_coordinates(right[0], right[1])
+        left_deep = [depth_coord_left[0], depth_coord_left[1]]
+        right_deep = [depth_coord_right[0], depth_coord_right[1]]
+        distance_2d = calculate_2d_distance(left_deep, right_deep)
         return distance_2d
-
-
 
     def draw_labels(self, boxes, confs, class_ids, centers, img): 
         indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
@@ -121,14 +132,12 @@ class YoloNet:
                 center = centers[i]
                 left = lefts[i]
                 right = rights[i]
-                depth_coords_center = dc.ssd_get_depth_coordinates(center[0], center[1])
-                depth_coord_left = dc.ssd_get_depth_coordinates(left[0], left[1])
-                depth_coord_right = dc.ssd_get_depth_coordinates(right[0], right[1])
+                depth_coords_center = self.dc.ssd_get_depth_coordinates(center[0], center[1])
+                depth_coord_left = self.dc.ssd_get_depth_coordinates(left[0], left[1])
+                depth_coord_right = self.dc.ssd_get_depth_coordinates(right[0], right[1])
                 print("dpth = ", depth_coords_center[2])
                 print("x = ", depth_coords_center[0])
                 print("y = ", depth_coords_center[1])
-                #count = cv2.cuda.getCudaEnabledDeviceCount()
-                #print(count)
                 left_deep = [depth_coord_left[0], depth_coord_left[1]]
                 right_deep = [depth_coord_right[0], depth_coord_right[1]]
                 distance_2d = self.calculate_2d_distance(left_deep, right_deep)
@@ -144,61 +153,90 @@ class YoloNet:
                 cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
         cv2.imshow("YOLO - Tiny v3", img)
 
-    def image_detect(sefl, img_path): 
-        #model, classes, colors, output_layers = load_yolo()
-        image, height, width, channels = self.load_image(img_path)
-        blob, outputs = self.detect_objects(image)
-        boxes, confs, class_ids, centers = self.get_box_dimensions(outputs, height, width)
-        self.draw_labels(boxes, confs, class_ids, center, image)
-        while True:
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
+    def calculate_depth(self, boxes, confs, class_ids, centers, lefts, rights, img): 
+        indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(self.classes[class_ids[i]])
+                color = self.colors[i]
+                center = centers[i]
+                left = lefts[i]
+                right = rights[i]
+                depth_coords_center = self.dc.ssd_get_depth_coordinates(center[0], center[1])
+                depth_coord_left = self.dc.ssd_get_depth_coordinates(left[0], left[1])
+                depth_coord_right = self.dc.ssd_get_depth_coordinates(right[0], right[1])
+                print("object = ",label)
+                print("dpth = ", depth_coords_center[2])
+                print("x = ", depth_coords_center[0])
+                print("y = ", depth_coords_center[1])
+                left_deep = [depth_coord_left[0], depth_coord_left[1]]
+                right_deep = [depth_coord_right[0], depth_coord_right[1]]
+                distance_2d = self.calculate_2d_distance(left_deep, right_deep)
+        cv2.imshow("YOLO - Tiny v3", img)
 
-    def webcam_detect(self):
-        #model, classes, colors, output_layers = load_yolo()
+
+    def normal_detection(self):
         cap = self.start_webcam()
+        self.start = time.time()
         while True:
+            self.start_time = time.time()
             _, frame = cap.read()
             height, width, channels = frame.shape
             blob, outputs = self.detect_objects(frame)
             boxes, confs, class_ids, centers= self.get_box_dimensions(outputs, height, width)
             self.draw_labels(boxes, confs, class_ids, centers, frame)
+            print("FPS: ", 1.0 / (time.time() - self.start_time))
             key = cv2.waitKey(1)
             if key == 27:
+                break
+            if (time.time() - self.start) > 10:
+                print("10 SEC OVER")
                 break
         cap.release()
-
-
-    def depth_detect(self, dc):
-        #model, classes, colors, output_layers = load_yolo()
-        counter = 0 
-        start_time = time.time()
-        x = 1 # displays the frame rate every 1 second
-        counter = 0
+    
+    def depth_headless(self):
+        self.init_depth_camera()
+        color_frame, depth_frame_distance = self.dc.ssd_get_frame()
+        self.start = time.time()
         while True:
-            color_frame, depth_frame_distance = dc.ssd_get_frame()
-            frame = color_frame
-            height, width, channels = frame.shape
-            blob, outputs = self.detect_objects(frame)
-            count = cv2.cuda.getCudaEnabledDeviceCount()
-            print(count)
-            boxes, confs, class_ids, centers, lefts, rights = self.get_box_dimensions(outputs, height, width)
-            self.draw_labels_depth(boxes, confs, class_ids, lefts, lefts, rights, frame)
-            counter+=1
-            if (time.time() - start_time) > x :
-                print("FPS: ", counter / (time.time() - start_time))
-                counter = 0
-                start_time = time.time()
+            self.start_time = time.time()
+            color_frame, depth_frame_distance = self.dc.ssd_get_frame()
+            height, width, channels = color_frame.shape
+            blob, outputs = self.detect_objects(color_frame)
+            boxes, confs, class_ids, centers, lefts, rights = self.get_box_dimensions_depth(outputs, height, width)
+            self.calculate_depth(boxes, confs, class_ids, lefts, lefts, rights, color_frame)
+            print("FPS: ", 1.0 / (time.time() - self.start_time)) 
             key = cv2.waitKey(1)
-
             if key == 27:
                 break
+            if (time.time() - self.start) > 10:
+                print("10 SEC OVER")
+                break
+        self.dc.release()
+
+    def depth_display(self):
+        self.init_depth_camera()
+        self.start = time.time()
+        while True:
+            self.start_time = time.time() 
+            color_frame, depth_frame_distance = self.dc.ssd_get_frame()
+            height, width, channels = color_frame.shape
+            blob, outputs = self.detect_objects(color_frame)
+            boxes, confs, class_ids, centers, lefts, rights = self.get_box_dimensions_depth(outputs, height, width)
+            self.draw_labels_depth(boxes, confs, class_ids, lefts, lefts, rights, color_frame)
+            print("FPS P: ", 1.0 / (time.time() - self.start_time)) 
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+            if (time.time() - self.start) > 10:
+                print("10 SEC OVER")
+                break
+        self.dc.release()
         #cap.release()
     
 
 if __name__ == '__main__':
-    dc = DepthCamera()
     yolo = YoloNet()
-    yolo.depth_detect(dc)
+    yolo.normal_detection()
     cv2.destroyAllWindows()
